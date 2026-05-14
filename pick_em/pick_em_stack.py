cd4491a2b9b76e0a4d1ddfa9d37f5d502a9b711d
loew-tech <loew.technology@gmail.com>
2026-05-14 13:32:28 -0600

    stack

--- pick_em/pick_em_stack.py
+++ pick_em/pick_em_stack.py
@@ -1,19 +1,35 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_dynamodb as dynamodb,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+
 
 class PickEmStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        table = dynamodb.Table(
+            self, "PickEmTable",
+            table_name="PickEmTable",
+            partition_key=dynamodb.Attribute(
+                name='user_id',
+                type=dynamodb.AttributeType.STRING
+            ),
+            sort_key=dynamodb.Attribute(
+                name='created_at',
+                type=dynamodb.AttributeType.NUMBER
+            ),
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "PickEmQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        table.add_global_secondary_index(
+            index_name="category",
+            partition_key=dynamodb.Attribute(
+                name='category_id',
+                type=dynamodb.AttributeType.STRING
+            )
+        )
